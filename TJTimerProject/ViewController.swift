cc8dc66f7af2a7c660d792fc73bd775a00d20760
Kim Hyung Gyu <khkpoo@gmail.com>
2018-12-23 13:41:05 +0900

    Initial Commit

---
+++ TJTimerProject/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TJTimerProject
+//
+//  Created by Kim Hyung Gyu on 23/12/2018.
+//  Copyright © 2018 Kim Hyung Gyu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
